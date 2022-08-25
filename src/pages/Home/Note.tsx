f49ad56a77e84de767b49ef527ea20561eaf4e12
Lázaro Sánchez <lazarosanchezjs@gmail.com>
2022-08-24 22:53:52 -0300

    Add: markup for home page

--- src/pages/Home/Note.tsx
+++ src/pages/Home/Note.tsx
@@ -1,19 +1,46 @@
 
 import {NoteType} from 'types'
 import './styles/Note.scss'
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import dots from 'assets/images/dots.svg'
+import React, { useState } from 'react'
 
+type Elements = HTMLImageElement | HTMLElement
 export default function Note ({title, content} : NoteType) {
+  const navigate = useNavigate()
+  const [options, toggleOptions] = useState<boolean>(false)
+
+
+  const handleClick = (e: React.SyntheticEvent) =>{
+    const target: Elements = e.target as Elements
+    if (target.classList.contains('option')) {
+      toggleOptions(prev => !prev)
+    } else if (target.classList.contains('options-box')) {
+      return
+    } else if (target.classList.contains('single')) {
+      return
+    } else {
+      navigate('/home/:user/note/:id')
+    }
+  }
+
   return (
-    <Link to={'/home/:user/note/:id'}>
-      <div className='box'>
+      <div onClick={handleClick} className='box'>
         <span className='title'>
           <h4>{title}</h4>
         </span>
         <span className='content'>
           {content}
         </span>
+        <img src={dots} alt="Options" className='option' />
+        {
+          options ? 
+          <div className='options-box'>
+            <p className='single'>Delete</p>
+            <p className='single'>Archive</p>
+          </div>
+                  : ''
+        }
       </div>
-    </Link>
   )
 }
--- src/pages/Preview/NavList.tsx
+++ src/pages/Preview/NavList.tsx
@@ -5,6 +5,7 @@ export default function NavList () {
   return (
       <ul className='navlist'>
         <Link to='/register'><li>Get started</li></Link>
+        <Link to='/login'><li>Log In</li></Link>
         <a href="#mission"><li>Why us?</li></a>
         <a href="#pricing"><li>Pricing and contact us</li></a>
       </ul>
